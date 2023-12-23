872d5f1eafc7001fa9570121129ff5acfa8f2d38
Marianna <maryanarez@gmail.com>
2023-12-23 22:47:30 +0100

    Follow Mosh tutorial 01:05

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Message from "./components/Message";
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+
+function App() {
+  return (
+    <div>
+      <Alert>Hello World</Alert>
+    </div>
+  );
+}
+export default App;
