c68d0ab7f6345474e631e170691cda8c8a42550f
chasacademy-maryana-reznychenko <maryanarez@gmail.com>
2023-12-27 22:22:05 +0100

    Added Alert & Button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Message from "./components/Message";
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
+ const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <Alert>Hello World</Alert>
+      {alertVisible && <Alert onClose={()=> setAlertVisibility(false)}>My alert</Alert>}
+      <Button onClick={()=> setAlertVisibility(true)}>My Button</Button>
     </div>
   );
 }
